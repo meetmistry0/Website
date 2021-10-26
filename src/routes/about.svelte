50d3683bfce687262ded25109b7c4eaf334a8e07
meetmistry0 <meet@outlook.com>
2021-10-26 19:17:40 +0530

    Create about.svelte

---
+++ src/routes/about.svelte
@@ -0,0 +1,13 @@
+<script>
+    import DarkMode from "svelte-dark-mode";
+    let theme;
+</script>
+
+<svelte:head>
+    <title>About | Meet Mistry</title>
+    <meta name="description" content="About Me" />
+</svelte:head>
+
+<DarkMode bind:theme />
+
+<h1 class="page-title">About Me</h1>
