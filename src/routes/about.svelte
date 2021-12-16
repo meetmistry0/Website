9298d8a1f6bd3bb75773ca1dbc89a19a95e430a9
meetmistry0 <meet@outlook.com>
2021-12-16 18:06:17 +0530

    update deps and create about page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,1 +1,8 @@
-<h1>About</h1>
+<svelte:head>
+    <title>About | Meet Mistry</title>
+    <meta name="description" content="About Me" />
+</svelte:head>
+
+<main>
+    <h1 class="page-title">About</h1>
+</main>
