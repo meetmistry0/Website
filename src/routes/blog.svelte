217df4a0473acea2329fb0640a0b725d5e1df585
meetmistry0 <meet@outlook.com>
2022-01-09 15:04:58 +0530

    initial commit

--- src/routes/blog.svelte
+++ src/routes/blog.svelte
@@ -1,36 +1,8 @@
-<script context="module">
-    export async function load({ fetch }) {
-        const posts = await fetch("/blog.json").then((res) => res.json());
-
-        return {
-            props: {
-                posts,
-            },
-        };
-    }
-</script>
-
-<script>
-    import Article from "$lib/components/Article.svelte";
-    import ArticleTitle from "$lib/components/ArticleTitle.svelte";
-    import ArticleMeta from "$lib/components/ArticleMeta.svelte";
-    import ArticleDescription from "$lib/components/ArticleDescription.svelte";
-    export let posts;
-</script>
-
 <svelte:head>
     <title>Blog | Meet Mistry</title>
     <meta name="description" content="Personal blog" />
 </svelte:head>
 
 <main>
-    <h1 class="page-title">Posts</h1>
-
-    {#each posts as { slug, title, description, date }}
-        <Article>
-            <ArticleTitle {slug} {title} />
-            <ArticleMeta {date} />
-            <ArticleDescription {description} {slug} />
-        </Article>
-    {/each}
+    <h1 class="page-title">Writings</h1>
 </main>
--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,11 @@
-import { mdsvex } from "mdsvex";
-import mdsvexConfig from "./mdsvex.config.js";
-import adapter from "@sveltejs/adapter-static";
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  extensions: [".svelte", ...mdsvexConfig.extensions],
-
-  kit: {
-    target: "#svelte",
-    adapter: adapter({
-      pages: "build",
-      assets: "build",
-      fallback: null,
-    }),
-  },
-
-  preprocess: [mdsvex(mdsvexConfig)],
+	kit: {
+		adapter: adapter(),
+		target: '#svelte'
+	}
 };
 
 export default config;
