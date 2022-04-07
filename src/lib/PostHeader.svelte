a2281d3b0e848f78e1f3e25b22c52a1d12909a4e
meetmistry0 <meet@outlook.com>
2022-04-07 22:00:21 +0530

    Update tab size

--- src/lib/PostHeader.svelte
+++ src/lib/PostHeader.svelte
@@ -1,14 +1,14 @@
 <script>
-    import BackToTop from "./BackToTop.svelte";
-    export let title;
-    export let description;
-    export let date;
-    export let read_time;
+  import BackToTop from "./BackToTop.svelte";
+  export let title;
+  export let description;
+  export let date;
+  export let read_time;
 </script>
 
 <svelte:head>
-    <title>Blog | {title}</title>
-    <meta name="description" content={description} />
+  <title>Blog | {title}</title>
+  <meta name="description" content={description} />
 </svelte:head>
 
 <h1>{title}</h1>
@@ -16,26 +16,26 @@
 <BackToTop />
 
 <style>
-    h1 {
-        color: var(--color-accent);
-        margin-bottom: 0;
-    }
+  h1 {
+    color: var(--color-accent);
+    margin-bottom: 0;
+  }
 
-    .meta {
-        margin: 0;
-        color: var(--color-secondary);
-        font-weight: bold;
-    }
+  .meta {
+    margin: 0;
+    color: var(--color-secondary);
+    font-weight: bold;
+  }
 
-    :global(p > a) {
-        border-bottom: 0.05em solid var(--color-accent);
-        box-shadow: inset 0 -0.05em 0 var(--color-accent);
-        color: inherit;
-        transition: background-color 0.25s cubic-bezier(0.33, 0.66, 0.66, 1);
-        text-decoration: none;
-    }
+  :global(p > a) {
+    border-bottom: 0.05em solid var(--color-accent);
+    box-shadow: inset 0 -0.05em 0 var(--color-accent);
+    color: inherit;
+    transition: background-color 0.25s cubic-bezier(0.33, 0.66, 0.66, 1);
+    text-decoration: none;
+  }
 
-    :global(p > a:hover, p > a:focus, p > a:active) {
-        background-color: var(--color-accent);
-    }
+  :global(p > a:hover, p > a:focus, p > a:active) {
+    background-color: var(--color-accent);
+  }
 </style>
--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,15 +1,15 @@
 <svelte:head>
-    <title>About | Meet Mistry</title>
-    <meta name="description" content="About Me" />
+  <title>About | Meet Mistry</title>
+  <meta name="description" content="About Me" />
 </svelte:head>
 
 <main>
-    <h1 class="page-title">About Me</h1>
-    <p>
-        I like to learn, experiment, do a deep dive into new technologies. Based
-        on what I have learnt, I want to create open-source applications and
-        contribute to the open-source software community.
-    </p>
+  <h1 class="page-title">About Me</h1>
+  <p>
+    I like to learn, experiment, do a deep dive into new technologies. Based on
+    what I have learnt, I want to create open-source applications and contribute
+    to the open-source software community.
+  </p>
 
-    <p>My hobbies are reading, cycling, listening to music and gaming</p>
+  <p>My hobbies are reading, cycling, listening to music and gaming</p>
 </main>
